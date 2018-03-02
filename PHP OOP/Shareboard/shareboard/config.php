7a4a64343f5c54b4828018aa33aeea30dcb5a28e
Rumen Topalov <topalovr@gmail.com>
2018-03-02 10:49:27 +0200

    PHP Projects

--- PHP OOP/Shareboard/shareboard/config.php
+++ PHP OOP/Shareboard/shareboard/config.php
@@ -2,7 +2,7 @@
 
 // Define DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "_YPOR_USER_");
+define("DB_USER", "_YOUR_USER_");
 define("DB_PASS", "_YOUR_PASS_");
 define("DB_NAME", "_YOUR_DBNAME_");
 
