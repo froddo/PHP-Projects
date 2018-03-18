5d86b3d54cad36d7c6e7f4af9ed13f1cb20c285f
Rumen Topalov <topalovr@gmail.com>
2018-03-18 19:44:38 +0200

    PHP OOP Ajax Projects

--- PHP OOP/AjaxAddresbook/addressbook/config/config.php
+++ PHP OOP/AjaxAddresbook/addressbook/config/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'frodo');
-define('DB_PASS', 'qwerty');
-define('DB_NAME', 'addressbook');
+define('DB_USER', 'YOUR_USER');
+define('DB_PASS', 'YOUR_PASS');
+define('DB_NAME', 'YOUR_NAME');
 
